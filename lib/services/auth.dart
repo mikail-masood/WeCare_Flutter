621c2bcea15bec098aad130fc0124f8c6e47a942
Mikail <mikail.masood@mail.utoronto.ca>
2020-12-17 17:08:11 -0500

    removed nonsense

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -1,11 +1,1 @@
-import 'package:wecare_2/models/user_info.dart';
 
-class AuthService {
-  UserStatus _checkUserStatus(status) {
-    return status != null ? UserStatus(status: status) : null;
-  }
-
-  Stream<UserStatus> get status {
-    return status;
-  }
-}
