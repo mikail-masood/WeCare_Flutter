d57ae75a1d3dda521839ca4fb7164f6bc81fdb3b
Mikail <mikail.masood@mail.utoronto.ca>
2020-12-25 15:59:42 -0500

    set up stream and path to sign in page

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
-import 'authenticate/stateSelect.dart';
+import 'package:provider/provider.dart';
+import 'package:wecare_2/models/user.dart';
+import 'package:wecare_2/services/auth.dart';
+import 'package:wecare_2/signInWrapper.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,9 +12,11 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Selector(),
-    );
+    return StreamProvider<User>.value(
+        value: AuthService().user,
+        child: MaterialApp(
+          debugShowCheckedModeBanner: false,
+          home: SignInWrapper(),
+        ));
   }
 }
