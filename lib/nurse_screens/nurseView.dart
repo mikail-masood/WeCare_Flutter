8a5e57c2dad568f8832b820b1cd4e31df77d8975
Mikail <mikail.masood@mail.utoronto.ca>
2020-12-25 16:03:04 -0500

    folders for the different uis

---
+++ lib/nurse_screens/nurseView.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class NurseView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(color: Colors.pink);
+  }
+}
